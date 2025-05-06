c5a145a21c59168cbb1b2d8769b511878cfee364
Víctor Pérez <victorperezvl95@gmail.com>
2025-05-06 16:48:00 +0200

    Creación del authMiddleware

---
+++ middlewares/auth.middlewares.js
@@ -0,0 +1,19 @@
+import { verifyToken } from '../config/jwt.js';
+
+export function authMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ error: 'Token no proporcionado' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = verifyToken(token);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ error: 'Token inválido o expirado' });
+  }
+}
