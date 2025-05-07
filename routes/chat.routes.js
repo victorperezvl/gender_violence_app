e870e412b1d12072633080ab2c9bcf958be4323a
Víctor Pérez <victorperezvl95@gmail.com>
2025-05-07 12:08:13 +0200

    rutas para el historial

--- routes/chat.routes.js
+++ routes/chat.routes.js
@@ -2,7 +2,13 @@ import express from 'express';
 const router = express.Router();
 import { handleChat } from '../controllers/chat.controller.js';    
 import { authMiddleware } from '../middlewares/auth.middlewares.js';
+import { getHistory, deleteHistory } from '../controllers/history.controller.js';
 
+// Chat routes
 router.post('/chat', authMiddleware, handleChat); 
+
+// History routes
+router.delete('/history', authMiddleware, deleteHistory);
+router.get('/history', authMiddleware, getHistory);
 
 export default router;
