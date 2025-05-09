8c2e8808dcc199e6f4ca89e109f02cf99770161a
Víctor Pérez <victorperezvl95@gmail.com>
2025-05-09 10:22:29 +0200

    login screen

---
+++ frontend/src/screens/loginScreen.js
@@ -0,0 +1,29 @@
+// src/screens/LoginScreen.js
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const LoginScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Pantalla de Login</Text>
+      <Button
+        title="Ir al Chat"
+        onPress={() => navigation.navigate('Chat')}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+});
+
+export default LoginScreen;
