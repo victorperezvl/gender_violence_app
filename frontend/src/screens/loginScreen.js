22b4d980f213e28c2eacaf403af6cc2ef65d2142
Víctor Pérez <victorperezvl95@gmail.com>
2025-05-12 17:12:01 +0200

    creando pantalla de login

--- frontend/src/screens/loginScreen.js
+++ frontend/src/screens/loginScreen.js
@@ -1,15 +1,54 @@
-// src/screens/LoginScreen.js
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
 
 const LoginScreen = ({ navigation }) => {
+  // Estado para el email y la contraseña
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Función para manejar el inicio de sesión
+  const handleLogin = () => {
+    // Aquí puedes agregar la lógica para autenticar al usuario
+    // Por ejemplo, llamar a tu servicio de autenticación
+    console.log('Email:', email, 'Password:', password);
+    
+    // Si el login es exitoso, redirigimos a la pantalla principal (Home)
+    navigation.navigate('Home');
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Pantalla de Login</Text>
-      <Button
-        title="Ir al Chat"
-        onPress={() => navigation.navigate('Chat')}
-      />
+      <View style={styles.formContainer}>
+        <Image
+        source={require('../../assets/icono_violencia.png')} 
+        style={styles.logo}
+        />
+        <Text style={styles.title}>Iniciar sesión</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Correo electrónico"
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+        />
+        
+        <TextInput
+          style={styles.input}
+          placeholder="Contraseña"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Iniciar sesión</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+          <Text style={styles.link}>¿No tienes cuenta? Regístrate</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -18,12 +57,55 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#6A0DAD',
+  },
+  logo: {
+    width: 200, 
+    height: 200,
+    alignSelf: 'center',
+    marginBottom: 40,
+    color: 'white',
+  },
+  formContainer: {
+    width: '98%',
+    justifyContent: 'center',
+    backgroundColor: '#854DBF',
+    borderRadius: 20,
+    padding: 20        
   },
   title: {
-    fontSize: 20,
-    marginBottom: 20,
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 30,
+    color: '#fff',
+  },
+  input: {
+    height: 50,
+    backgroundColor: '#B57EDC',
+    borderRadius: 10,
+    marginBottom: 15,
+    paddingLeft: 10,
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: '#007bff',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  link: {
+    color: '#fff',
+    textAlign: 'center',
+    marginTop: 15,
   },
 });
 
 export default LoginScreen;
+
