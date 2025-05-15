afcd3438ea3b7b31b6d163ab2c5b4d515ce5d91d
Víctor Pérez <victorperezvl95@gmail.com>
2025-05-15 17:15:29 +0200

    bug prop onScrollToResources solucionado

--- frontend/src/components/BoxHome.js
+++ frontend/src/components/BoxHome.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-const BoxHome = ( onScrollToResources ) => {
+const BoxHome = ( {onScrollToResources} ) => {
 
   const navigation = useNavigation();
   const openChatWithPrompt = (prompt) => {
