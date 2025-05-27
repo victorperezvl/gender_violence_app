9c0cbb8362a311f3b9c9017d91690b07d8f15fd8
Víctor Pérez <victorperezvl95@gmail.com>
2025-05-27 20:29:57 +0200

    instalación eas

--- frontend/src/components/TopBar.js
+++ frontend/src/components/TopBar.js
@@ -11,7 +11,7 @@ const TopBar = ({ onHelpPress }) => {
         resizeMode="contain"
       />
       <TouchableOpacity style={styles.loginButton} onPress={onHelpPress}>
-        <Text style={styles.helpButtonText}>Iniciar sesión</Text>
+        <Text style={styles.helpButtonText}>Login</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.helpButton} onPress={onHelpPress}>
         <Text style={styles.loginButtonText}>Ayuda</Text>
